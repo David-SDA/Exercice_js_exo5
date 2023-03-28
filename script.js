affadb4ea9ac1f58653df500dd1c8c9136ec13fc
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 11:55:20 +0200

    Ajout de la conversion en temps réel

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let nombre = document.getElementById("valeur");
-let francs = document.getElementsByClassName("francs");
+let francs = document.getElementById("franc");
 
 nombre.addEventListener("input", function(){
     let inputTextarea = nombre.value;
     if(isNaN(inputTextarea)){
         alert("Il faut une valeur numérique");
+    }
+    else{
+        calcul = inputTextarea * 6.55957;
+        resultat = calcul.toFixed(2);
+        francs.innerHTML = resultat;
     }
 })
